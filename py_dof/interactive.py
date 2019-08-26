e7f8b46e109cef54213bcbefad820161015e409e
rlaplaza <laplazasolanas@gmail.com>
2019-08-26 19:07:59 +0200

    Formatted code with Black, added new print function.

--- py_dof/interactive.py
+++ py_dof/interactive.py
@@ -3,10 +3,10 @@ from py_dof.moegradients import *
 
 
 def print_imoe_gradients_4p(
-    mole, one_dm, mf, occs, energies, maxstep=0.0001, verbose=0, moegrads=None
+    mole, one_dm, mf, occs, energies, maxstep=0.000001, verbose=0, moegrads=None
 ):
     if moegrads == None:
-        moegrads = calc_moe_gradients_4p(
+        moegrads = calc_moe_gradients_i4p(
             mole, one_dm, mf, occs, energies, maxstep, verbose
         )
     for j in range(0, np.count_nonzero(occs)):
@@ -20,7 +20,7 @@ def print_imoe_gradients_4p(
 
 
 def print_imoe_gradients_2p(
-    mole, one_dm, mf, occs, energies, maxstep=0.00005, verbose=0, moegrads=None
+    mole, one_dm, mf, occs, energies, maxstep=0.0000005, verbose=0, moegrads=None
 ):
     if moegrads == None:
         moegrads = calc_moe_gradients_2p(
@@ -34,3 +34,28 @@ def print_imoe_gradients_2p(
         )
         for i in range(0, mole.natm):
             print(mole.atom[i, 0], np.around(moegrads[i][j, :], 6) * (-1))
+
+
+def print_imoe_gradients_6p(
+    mole, one_dm, mf, occs, energies, maxstep=0.000001, verbose=0, moegrads=None
+):
+    if moegrads == None:
+        moegrads = calc_moe_gradients_i6p(
+            mole, one_dm, mf, occs, energies, maxstep, verbose
+        )
+    for j in range(0, np.count_nonzero(occs)):
+        print(
+            "(Inverse) Nuclear Gradients (x,y,z) of Molecular Orbital {0} with Energy {1} a.u. :".format(
+                j + 1, np.around(energies[j], 6)
+            )
+        )
+        for i in range(0, mole.natm):
+            print(mole.atom[i, 0], np.around(moegrads[i][j, :], 6) * (-1))
+
+
+def print_gradients(mole, one_dm, mf, coeffs, occs, energies, grads=None):
+    if grads == None:
+        grads = calc_gradients(mole, one_dm, mf, coeffs, occs, energies)
+    print("Nuclear Gradients (x,y,z) of Total Energy:")
+    for i in range(0, mole.natm):
+        print(mole.atom[i, 0], np.around(grads[i], 6))
