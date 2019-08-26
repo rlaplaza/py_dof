66b8b97e752061f86725977b96397547842d270c
rlaplaza <laplazasolanas@gmail.com>
2019-08-26 23:39:44 +0200

    Forgot to add printing option. Duh.

--- py_dof/interactive.py
+++ py_dof/interactive.py
@@ -54,6 +54,23 @@ def print_imoe_gradients_6p(
             print(mole.atom[i, 0], np.around(moegrads[i][j, :], 6) * (-1))
 
 
+def print_imoe_gradients_np(
+    mole, one_dm, mf, occs, energies, maxstep=0.000001, verbose=0, moegrads=None
+):
+    if moegrads == None:
+        moegrads = calc_moe_gradients_np(
+            mole, one_dm, mf, occs, energies, maxstep, verbose
+        )
+    for j in range(0, np.count_nonzero(occs)):
+        print(
+            "(Inverse) Nuclear Gradients (x,y,z) of Molecular Orbital {0} with Energy {1} a.u. :".format(
+                j + 1, np.around(energies[j], 6)
+            )
+        )
+        for i in range(0, mole.natm):
+            print(mole.atom[i, 0], np.around(moegrads[i][j, :], 6) * (-1))
+
+
 def print_gradients(mole, one_dm, mf, coeffs, occs, energies, grads=None):
     if grads == None:
         grads = calc_gradients(mole, one_dm, mf, coeffs, occs, energies)
